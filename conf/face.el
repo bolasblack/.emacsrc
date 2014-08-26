100feeb2955d3d20079cde49443e0dc973a26ff1
bolasblack <bolasblack@gmail.com>
2014-08-26 12:36:47 +0800

    change(conf/face): 某些设置只在窗口环境中生效

--- conf/face.el
+++ conf/face.el
@@ -3,27 +3,33 @@
 ;;;; 外观相关
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;关闭起动时的那个“开机画面”。
-(setq inhibit-startup-message t)
+(if window-system
+    (progn
+      ;; 控制是否显示行号和内容容器直接的间隔
+      (set-fringe-style 0)
 
-;; 控制是否显示行号和内容容器直接的间隔
-(set-fringe-style 0)
+      ;; 控制 tip 是否在 minibuffer 显示（-1 为显示）
+      (tooltip-mode t)
+
+      ;; 没有 toolbar
+      (tool-bar-mode -1)
+
+      ;; 光标不闪，不恍花眼睛
+      (blink-cursor-mode -1)
+      (transient-mark-mode t)
+
+      ;; 没有滚动条 24.1
+      (scroll-bar-mode -1)
+      ))
 
 ;; 调整行号栏的格式
 (setq linum-format "%3d ")
 
-;; 控制 tip 是否在 minibuffer 显示（-1 为显示）
-(tooltip-mode t)
+;; 关闭启动时的 “开机画面”
+(setq inhibit-startup-message t)
 
-;; 没有 toolbar
-(tool-bar-mode -1)
-
-;; 没有 menubar,用 f12 呼出
+;; 没有 menubar
 (menu-bar-mode -1)
-
-;; 光标不闪，不恍花眼睛
-(blink-cursor-mode -1)
-(transient-mark-mode t)
 
 ;; 括号匹配时显示另外一边的括号，而不是烦人的跳到另一个括号。
 (show-paren-mode t)
@@ -35,14 +41,8 @@
 ;; 显示列号
 (column-number-mode t)
 
-;; 没有滚动条 24.1
-(scroll-bar-mode -1)
-
-;; 行号 emacs23 支持
+;; 显示行号
 (global-linum-mode t)
-
-;; folding
-;; (folding-mode t)
 
 ;;语法加亮
 (global-font-lock-mode t)
@@ -56,10 +56,10 @@
 ;; 时间的格式
 (setq display-time-format "%H:%M @ %m.%d")
 
-;;时间的变化频率
+;; 时间的变化频率
 (setq display-time-interval 10)
 
-;;在标题栏显示buffer的名字，而不是 emacs@email.***这样没用的提示。
+;; 在标题栏显示buffer的名字，而不是 emacs@email.***这样没用的提示。
 (setq frame-title-format "Emacs@%b")
 
 (delete-selection-mode -1)
