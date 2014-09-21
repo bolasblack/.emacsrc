a43a2bd3e622a5763ca0d26068c62aded66dbdc1
bolasblack <bolasblack@gmail.com>
2014-09-22 00:06:01 +0800

    feat(lisps/sudo): 增加了一些 sudo 编辑的命令

---
+++ lisps/sudo.el
@@ -0,0 +1,21 @@
+(defun sudo-save (&optional arg)
+    "Save currently visited file as root.
+
+With a prefix ARG prompt for a file to visit.
+Will also prompt for a file to visit if current
+buffer is not visiting a file."
+  (interactive "P")
+  (if (or arg (not buffer-file-name))
+      (write-file (concat "/sudo:root@localhost:" (read-file-name "File:")))
+    (write-file (concat "/sudo:root@localhost:" buffer-file-name))))
+
+(defun sudo-edit (&optional arg)
+  "Edit currently visited file as root.
+
+With a prefix ARG prompt for a file to visit.
+Will also prompt for a file to visit if current
+buffer is not visiting a file."
+  (interactive "P")
+  (if (or arg (not buffer-file-name))
+      (find-file (concat "/sudo:root@localhost:" (read-file-name "Find file(as root): ")))
+    (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
