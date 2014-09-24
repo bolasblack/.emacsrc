5983e335fa886c9bcaffae7524c4ca3fd15f871f
bolasblack <bolasblack@gmail.com>
2014-09-24 18:40:40 +0800

    feat(): 增加了对二进制 plist 文件的支持

--- conf/mode-mapping.el
+++ conf/mode-mapping.el
@@ -21,3 +21,21 @@
 ;; Lua
 (add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
 (add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
+
+;; OS X Plist
+
+;; Emacs provides jka-compr which decompresses a file to stdout for reading,
+;; and compresses the data from stdin to write the file back out again.
+
+;; Allow editing of binary .plist files.
+(add-to-list 'jka-compr-compression-info-list
+             ["\\.plist$"
+              "converting text XML to binary plist"
+              "plutil"
+              ("-convert" "binary1" "-o" "-" "-")
+              "converting binary plist to text XML"
+              "plutil"
+              ("-convert" "xml1" "-o" "-" "-")
+              nil nil "bplist"])
+;;It is necessary to perform an update!
+(jka-compr-update)
