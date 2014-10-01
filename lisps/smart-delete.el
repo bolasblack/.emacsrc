f0451b8b87b87694b579b0bfa831927f15a864bc
bolasblack <bolasblack@gmail.com>
2014-10-01 22:11:17 +0800

    feat(keyboard): 如果没有选区的话，C-w 不再会将删除的内容放入剪贴板历史

--- lisps/smart-delete.el
+++ lisps/smart-delete.el
@@ -1,7 +1,7 @@
 ;; -*- Emacs-Lisp -*-
 
 ;; From https://github.com/4DA/emacs-stuff/blob/20a5180eda54c76b7da3ecb1b0a640ea6c04c682/smart-kill.el
-(defun backward-kill-word-or-delim (arg &optional killp)
+(defun backward-delete-word-or-delim (arg &optional killp)
   "Deletes a word or delimiter/punctuation symbol backwards"
   (interactive "*p\nP")
   (push-mark (point))
@@ -17,14 +17,14 @@
         (unless (= 0 (skip-syntax-backward "-"))
           (return))
         (backward-delete-char-untabify arg killp)))
-  (kill-region (point) (mark))
+  (delete-region (point) (mark))
   (pop-mark))
 
-(defun forward-kill-word-or-delim (arg &optional killflag)
+(defun forward-delete-word-or-delim (arg &optional killflag)
   "Deletes a word or delimiter/punctuation symbol forward"
   (interactive "p\nP")
   (push-mark (point))
   (if (= 0 (skip-syntax-forward "w"))
       (delete-forward-char arg killflag))
-  (kill-region (point) (mark))
+  (delete-region (point) (mark))
   (pop-mark))
