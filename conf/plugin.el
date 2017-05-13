0a9387353aface1f9b7be49aa850f1c9f3076cb3
c4605 <bolasblack@gmail.com>
2017-05-13 14:41:29 +0800

    Parinfer 格式化代码

--- conf/plugin.el
+++ conf/plugin.el
@@ -211,8 +211,7 @@
 (use-package expand-region :ensure t)
 
 ;; 自动调整缩进
-(use-package aggressive-indent
-  :ensure t)
+(use-package aggressive-indent :ensure t)
 
 ;;;;;;;;;;;;;;;;;;;; 项目 ;;;;;;;;;;;;;;;;;;;;
 
@@ -308,8 +307,8 @@
                                ("css"        . css-mode)
                                ("html"       . html-mode)
                                ("javascript" . js-mode)
-                               ("jsx"        . js-mode)
-                               )))
+                               ("jsx"        . js-mode))))
+
           (-reduce-from (lambda (matched-mode piar)
                           (if (string= curr-lang (car piar))
                               (progn
@@ -319,18 +318,16 @@
                               (if (not (equal (cdr piar) matched-mode))
                                   (yas-deactivate-extra-mode (cdr piar)))
                               matched-mode)))
-                        nil lang-mode-map)
-          )))
-  )
+                        nil lang-mode-map)))))
+
+
 (use-package ledger-mode
   :ensure t
   :mode ("\\.beancount\\'" "\\.bean\\'")
   :init
   (add-hook 'ledger-mode-hook
             (lambda ()
-              (bind-key "C-M-i" 'ledger-magic-tab ledger-mode-map)
-              ))
-  )
+              (bind-key "C-M-i" 'ledger-magic-tab ledger-mode-map))))
 
 ;;;;;;;;;;;;;;;;;;;; 开发环境 ;;;;;;;;;;;;;;;;;;;;
 
@@ -362,12 +359,12 @@
   (progn
     (setq parinfer-extensions
           '(defaults       ; should be included.
-            pretty-parens  ; different paren styles for different modes.
-            evil           ; If you use Evil.
-            lispy          ; If you use Lispy. With this extension, you should install Lispy and do not enable lispy-mode directly.
-            paredit        ; Introduce some paredit commands.
-            smart-tab      ; C-b & C-f jump positions and smart shift with tab & S-tab.
-            smart-yank))   ; Yank behavior depend on mode.
+             pretty-parens  ; different paren styles for different modes.
+             evil           ; If you use Evil.
+             lispy          ; If you use Lispy. With this extension, you should install Lispy and do not enable lispy-mode directly.
+             paredit        ; Introduce some paredit commands.
+             smart-tab      ; C-b & C-f jump positions and smart shift with tab & S-tab.
+             smart-yank))   ; Yank behavior depend on mode.
     (add-hook 'clojure-mode-hook #'parinfer-mode)
     (add-hook 'emacs-lisp-mode-hook #'parinfer-mode)
     (add-hook 'common-lisp-mode-hook #'parinfer-mode)
